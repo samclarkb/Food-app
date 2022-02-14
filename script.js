04045ff23be2b78cd7d3f5b36169c0e0932aa12b
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-02-14 13:48:50 +0100

    second commit

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const dataOphalen = () => {
 	let barcode = 3168930158158
 	fetch('https://world.openfoodfacts.org/api/v0/product/' + barcode + '.json') // Dit is de link van de externe API
 		.then(response => response.json())
-		.then(data => console.log(data))
+		.then(data => {
+			naam(data)
+		})
 		.catch(err => {
 			console.error(err) // Als er iets mis gaat geeft deze catch een error weer in de console
 		})
+}
+
+const h2 = document.querySelector('h2')
+
+const naam = data => {
+	console.log(data)
+	console.log(data.product.abbreviated_product_name)
+	h2.innerHTML = data.product.abbreviated_product_name
 }
 
 dataOphalen()
